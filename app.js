a33d66ae6e1ed73875e7145e8a458f3afd59228c
Fabiano <fabiano.ti@unc.br>
2023-12-20 19:16:03 -0300

    cors

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express")
 const productsRouter = require("./routes/productsRouter")
 const mongoose = require('mongoose')
+const cors = require('cors')
 require('dotenv').config()
 
 const app = express()
 const port = 3000
 
+app.use(cors())
 app.use(express.json())
 app.use("/api/products", productsRouter)
 
